a437703c183fd1fdd32d95ab93a3bbbc4a2ea211
Артем <103676070+ArtemSmedliaiev@users.noreply.github.com>
2023-12-29 03:01:14 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -7,8 +7,8 @@ int main()
 {
     double d, alpha;
     
-    d = 14;
-    alpha = 180;
+    d = 10;
+    alpha = 30;
     double z1 = sin(PI / 2 + 3 * d) / (1 - sin(3 * alpha - PI)); //sin excepts args in radians
     double z2 = 1 / tan((5/4) * PI + (3/2) * alpha); //tan excepts args in radians
     
